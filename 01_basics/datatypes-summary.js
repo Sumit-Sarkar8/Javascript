21b3f73914ce369b4e6f8d34ae2bb79dee46cead
Sumit <141705692+Sumit-Sarkar8@users.noreply.github.com>
2023-08-10 08:19:38 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -39,5 +39,34 @@ console.log(typeof scoreValue);
 console.log(typeof myFunction); // we call function as object function and it's output is function
 console.log(typeof avengers); 
 
-console.table(["anotherId", "bigNumber", "outSideTemp", "scoreValue", "myFunction", "avengers"]);
+// console.table(["anotherId", "bigNumber", "outSideTemp", "scoreValue", "myFunction", "avengers"]);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ____________________________________________________________________________________________________
+
+// Memory in Javascript 
+
+// Stack (Primtive), Heap (Non-Primitive)
+
+let myYoutubename = "Maths";
+
+let anotherName =  "myYoutubename";
+anotherName = "Computerscience"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "sumit@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
